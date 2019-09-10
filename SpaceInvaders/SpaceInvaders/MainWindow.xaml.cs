f328b533c9fcaf16909aa46765905143a95e7b87
Eduard Pavliuc <epavliuc@spartaglobal.com>
2019-09-10 09:26:26 +0100

    Initial commit

---
+++ SpaceInvaders/SpaceInvaders/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpaceInvaders
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Player Control, left to right. + boundaries
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {          
+            double x = Canvas.GetLeft(player);
+            if (x > 0 & x < 600)
+            {
+                if (e.Key == Key.Left)
+                {
+                    Canvas.SetLeft(player, x -= 10);
+                }
+                else if (e.Key == Key.Right)
+                {
+                    Canvas.SetLeft(player, x += 10);
+                }
+            }else if (x <= 0)
+            {
+                Canvas.SetLeft(player, x + 10);
+            }
+            else if (x >= 600)
+            {
+                Canvas.SetLeft(player, x - 10);
+            }
+        }
+    }
+}
